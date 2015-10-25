7dcb7d6127019ab8dc4d656ceb25060a106ea62c
Daniel Prado <pradosan@mit.edu>
2015-10-25 19:17:05 -0400

    going back to two web apps

--- makeCalls.py
+++ makeCalls.py
@@ -36,8 +36,8 @@ callList = CallList()
 
 @app.route('/')
 def public_calls():
-    """Displays the pending calls"""
-    return render_template('layout.html', calls=callList.getCalls())
+	"""Displays the pending calls"""
+	return render_template('layout.html', calls=callList.getCalls())
 
 
 @app.route('/add_call', methods=['POST'])
@@ -50,23 +50,23 @@ def add_call():
 	timeToCall = request.form['time_to_call']
 
 	allEntriesAnswered = (callNumber and callFrom and accountSid and authToken and timeToCall)
-    if (allEntriesAnswered):
-        # Take date input and change to python datetime
-        call_date = datetime(*[int(v) for v in request.form['time_to_call'].replace('T', '-').replace(':', '-').split('-')])
-        
-        time_of_request=datetime.today()
+	if (allEntriesAnswered):
+		# Take date input and change to python datetime
+		call_date = datetime(*[int(v) for v in request.form['time_to_call'].replace('T', '-').replace(':', '-').split('-')])
+
+		time_of_request=datetime.today()
         
 
-        # Determines if the time the call is to executed is in the future.
-        # If the call is to be made in the future, the call goes through
-        # and is placed on a pending/completed list of calls to be made.
+		# Determines if the time the call is to executed is in the future.
+		# If the call is to be made in the future, the call goes through
+		# and is placed on a pending/completed list of calls to be made.
 
-        # This needs to be changed. Place this logic inside of make_call.
-        if call_date > time_of_request:
-        	# Finds the difference between the time the call was 
-        	# registered and when it is to be executed.
-        	delta_t=call_date-time_of_request
-        	secs=delta_t.seconds+1
+		# This needs to be changed. Place this logic inside of make_call.
+		if call_date > time_of_request:
+			# Finds the difference between the time the call was 
+			# registered and when it is to be executed.
+			delta_t=call_date-time_of_request
+			secs=delta_t.seconds+1
 
 
         	# Check if token and sid work
@@ -74,18 +74,17 @@ def add_call():
         	# If that's not an option, look into python callbacks
         	# or try catch method
 
-            callList.appendCall(callNumber, call_date)
-            t = Timer(secs, make_call(callNumber, callFrom, accountSid,authToken))
-            t.start()
-            flash('Your call was recorded')
-        else:
-            flash('Your call was not recorded. Please select some time in the future for your call to be made.')
-    else:
-        flash('Your call was not recorded. Please make sure all fields have an answer')
+			callList.appendCall(callNumber, call_date)
+			t = Timer(secs, make_call(callNumber, callFrom, accountSid,authToken))
+			t.start()
+			flash('Your call was recorded')
+		else:
+			flash('Your call was not recorded. Please select some time in the future for your call to be made.')
+	else:
+		flash('Your call was not recorded. Please make sure all fields have an answer')
     return redirect(url_for('public_calls'))
 
 def make_call(numberToCall, callFromNumber, SID, token):
     # To find these visit https://www.twilio.com/user/account
-    client = TwilioRestClient(SID, token)
-    call = client.calls.create(to=numberToCall, from_=callFromNumber,
-                           url='https://sheltered-temple-5934.herokuapp.com/')
+	client = TwilioRestClient(SID, token)
+	call = client.calls.create(to=numberToCall, from_=callFromNumber, url='https://sheltered-temple-5934.herokuapp.com/')
