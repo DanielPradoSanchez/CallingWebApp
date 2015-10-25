63f025b0896efc3012974f926db97c252da70368
Daniel Prado <pradosan@mit.edu>
2015-10-25 19:28:55 -0400

    Redeploying

--- makeCalls.py
+++ makeCalls.py
@@ -22,6 +22,7 @@ SECRET_KEY = 'development key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
+path_to_favicon = '/favicon.ico'
 
 # initialize empty call list
 callList = CallList()
