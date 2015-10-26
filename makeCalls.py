aed72573e946ce5932d655b2b4689f5baa50d49b
Daniel Prado <pradosan@mit.edu>
2015-10-26 11:21:38 -0400

    Commented

--- makeCalls.py
+++ makeCalls.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #
 #   A web app written with Flask and Twilio. The app allows
 #   you to call a phone and it automatically says the specified 
@@ -8,10 +7,6 @@
 #	are calling from (a number associated with the account), the
 #	number they are calling, and when the call is to be made (date and time)
 #
-
-#####
-##### Change time stuff to UTC (get the time of today in utc)
-#####
 
 import time
 from datetime import datetime
@@ -31,12 +26,13 @@ app.config.from_object(__name__)
 app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
 
 
+# Loads the web page
 @app.route('/')
 def public_calls():
-	# Displays the pending calls
 	return render_template('layout.html')
 
 
+# Handles registering a call
 @app.route('/add_call', methods=['POST'])
 def add_call():
 	# Get inputs from form
@@ -46,20 +42,25 @@ def add_call():
 	authToken = request.form['auth_token']
 	timeToCall = request.form['time_to_call']
 
-	#allEntriesAnswered = (callNumber and callFrom and accountSid and authToken and timeToCall)
+	# Check to see if any entries were left blank
 	if (callNumber and callFrom and accountSid and authToken and timeToCall):
 		# Take date input and change to python datetime
 		callTimeLocal = datetime(*[int(v) for v in timeToCall.replace('T', '-').replace(':', '-').split('-')])
 
 		timeOfRequestUTC=datetime.today()
 		
+		# Offset Eastern Standard Time to UTC
 		callTimeUTC = timedelta(0,14400) + callTimeLocal
 
+		# Make sure the requested time for the call to be made is in the future
 		if callTimeUTC > timeOfRequestUTC:
+
 			# Finds the difference between the time the call was 
 			# registered and when it is to be executed.
 			delta_t=callTimeUTC-timeOfRequestUTC
 			secs=delta_t.total_seconds()
+
+			# Sets up when the call is to be made
 			Timer(secs, make_call,[callNumber, callFrom, accountSid,authToken]).start()
 			flash('Your call has been recorded')
 			
@@ -72,4 +73,5 @@ def add_call():
 def make_call(numberToCall, callFromNumber, SID, token):
     # To find these visit https://www.twilio.com/user/account
 	client = TwilioRestClient(SID, token)
+	# The url field here leads to another web app (SpeechForCallingWebApp)
 	call = client.calls.create(to=numberToCall, from_=callFromNumber, url='https://sheltered-temple-5934.herokuapp.com/')
