662cd5f203ca74544586e5a0b3fad32b8f383545
Daniel Prado <pradosan@mit.edu>
2015-10-25 22:51:32 -0400

    trying to handle exception

--- makeCalls.py
+++ makeCalls.py
@@ -60,8 +60,7 @@ def add_call():
 		
 		callTimeUTC = timedelta(0,14400) + callTimeLocal
 
-		#if callTimeUTC > timeOfRequestUTC:
-		if False:
+		if callTimeUTC > timeOfRequestUTC:
 			flash('got here')
 			# Finds the difference between the time the call was 
 			# registered and when it is to be executed.
