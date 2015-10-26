42a89f351e2ae51c8a46967aeb307827d4ac37d2
Daniel Prado <pradosan@mit.edu>
2015-10-25 23:22:01 -0400

    trying to handle exception

--- makeCalls.py
+++ makeCalls.py
@@ -65,13 +65,15 @@ def add_call():
 			# registered and when it is to be executed.
 
 			delta_t=callTimeUTC-timeOfRequestUTC
+			flash(str(callTimeUTC))
+			flash(str(timeOfRequestUTC))
+			flash(str(delta_t))
 			secs=delta_t.seconds+1
 
 			callList.appendCall(callNumber, timeToCall)
 			t = Timer(secs, make_call(callNumber, callFrom, accountSid,authToken))
 			t.start()
-			#except twilio.TwilioRestException as e:
-			#flash('Your call was not recorded. Some of the information did not match an account.')
+			flash('Your call was not recorded. Some of the information did not match an account.')
 			
 		else:
 			flash('Your call was not recorded. Please select some time in the future for your call to be made.')
